a816f1959e6b7e737d9b91880fdd76f8f5ed4063
G00401655@atu.ie <G00401655@atu.ie>
2023-10-18 15:28:44 +0100

    Finished part 2, added FeignController, FeignService, TodoClient, TodoResponse

---
+++ src/main/java/ie/atu/week5lockingapp/FeignService.java
@@ -0,0 +1,25 @@
+package ie.atu.week5lockingapp;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeignService {
+
+    private final TodoClient todoClient;
+
+
+    public FeignService(TodoClient todoClient) {
+        this.todoClient = todoClient;
+    }
+
+    public TodoResponse fetchData(){
+        TodoResponse td = todoClient.fetchData();
+        System.out.println(td);
+        try{
+            Thread.sleep(2000);
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return td;
+    }
+}
